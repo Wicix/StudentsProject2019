1a96fe3dfb05234c6ce219e3f772a497707a0e6a
wicix <Wiktorj99@gmail.com>
2020-01-17 01:10:54 +0100

    Changing two function, playlist_df_function and str_his_with_playlist. Now without using loop for

--- BuszkaJacaszekJeszka/functions.r
+++ BuszkaJacaszekJeszka/functions.r
@@ -4,7 +4,7 @@ library(dplyr)
 library(tidyr)
 
 ##returns dataframe with data from spotify
-Streaming_History_df <- function(folder_path){
+streaming_history_df <- function(folder_path){
   file_paths<- list.files(folder_path,"StreamingHistory")
   read_files <- lapply(file_paths, jsonlite::fromJSON)
   bind_rows(read_files)
@@ -44,8 +44,8 @@ add_weekday <- function(streaming_history){
 }
 
 #### creating and preparing dataframe
-Streaming_History_Complete <- function(folder_path){
-  Streaming_History_df(folder_path) %>%
+streaming_history_complete <- function(folder_path){
+  streaming_history_df(folder_path) %>%
   names_change() %>% 
   mutate(end_time = ymd_hm(end_time)) %>% 
   mutate(s_played = dmilliseconds((s_played))) %>%
@@ -54,7 +54,7 @@ Streaming_History_Complete <- function(folder_path){
   add_weekday()
 }
 ### creating dataframe with date, devices and country 
-Search_Queries_df <- function(folder_path){
+search_queries_df <- function(folder_path){
   file_path <-list.files(folder_path,"SearchQueries")
   select(jsonlite::fromJSON(file_path),1:3) %>%
   mutate(date = ymd(date))
@@ -64,56 +64,50 @@ Search_Queries_df <- function(folder_path){
 
 ### creating dataframe with names of playlists, string containing song names separated 
 #by ";;;" and artist names separated by ";;;"
-Playlist_df_function <- function(folder_path){
-  file_path <-list.files(folder_path,"Playlist")
-  a <- jsonlite::fromJSON(file_path)
-  b <- select(a[[1]], name, items)
-  num_of_rows <- dim(b)[1]
-  vec_num_of_rows <- 1:num_of_rows
-  df <- data.frame(b$name,NA, NA)
-  colnames(df) <- c("Playlist names","Song names","Artist names")
-  for (i in vec_num_of_rows){
-    df[i,2] <- paste(b[i,2][[1]]$track$trackName, collapse = ";;;")
-    df[i,3] <- paste(b[i,2][[1]]$track$artistName, collapse = ";;;")
-  df[,1] <- as.character(df[,1])
-  }
+playlist_df_function <- function(folder_path) {
+  song_names_function <- function(x)
+    return(df[[2]][[x]][[1]][[1]])
   
-  return(df)
+  artist_names_function <- function(x)
+    return(df[[2]][[x]][[1]][[2]])
+  
+  file_path <- list.files(folder_path, "Playlist")
+  df <- jsonlite::fromJSON(file_path)
+  df <- select(df[[1]], name, items)
+  df <-
+    transmute(
+      df,
+      name,
+      "song_names" = sapply(1:nrow(df), song_names_function),
+      "artist_names" = sapply(1:nrow(df), artist_names_function)
+    )
 }
 
 # creating dataframe similar to Streaming_History_Complete, but this one has additional 
 #column with lists of playlists that including that song
-Playlist_df_Str_his <- function(folder_path){
-  Str_his_df <- Streaming_History_Complete(folder_path)
-  Playlist_df <- Playlist_df_function(folder_path)
-  In_which_playlist <- select(data.frame(1:dim(Str_his_df)[1],NA),2)
-  colnames(In_which_playlist) <- c("In_playlists")
-  for (i in 1:dim(Playlist_df)[1]){
-    splited_song_names <- strsplit(Playlist_df[i,2],";;;")
-    
-    splited_artist_names <- strsplit(Playlist_df[i,3],";;;")
-    
-    for (j in 1:dim(Str_his_df)[1]){
-      for(k in 1:length(splited_artist_names[[1]])){
-        if(Str_his_df[j,2]==splited_artist_names[[1]][k] & Str_his_df[j,3]==splited_song_names[[1]][k] )
-        {
-         if(is.na(In_which_playlist[j,1]==TRUE)){
-           In_which_playlist[j,1] <- Playlist_df[i,1]
-           break
-         }
-          else {In_which_playlist[j,1]<- paste(In_which_playlist[j,1], ";",Playlist_df[i,1])
-            break
-          }
-        }
-     }
+str_his_with_playlists <- function(folder_path) {
+  playlist_df <- playlist_df_function(folder_path)
+  str_his_comp <- streaming_history_complete(folder_path)
+  in_which_playlists <- function(song_row) {
+    in_playlist <- function(playlist_row) {
+      position_in_playlist <- function(position_number) {
+        if (((str_his_comp[song_row, 2] == playlist_df[playlist_row, 3][[1]][position_number])) &
+            (str_his_comp[song_row, 3] == playlist_df[playlist_row, 2][[1]][position_number]))
+          
+          return(playlist_df[playlist_row, 1])
+      }
+      unlist(unique(lapply(
+        1:length(playlist_df[playlist_row, 3][[1]]), position_in_playlist
+      )))
     }
+    if (is.null(unlist(sapply(1:length(playlist_df[, 1]), in_playlist))))
+      return("It is not in any playlist")
+    unlist(sapply(1:length(playlist_df[, 1]), in_playlist))
+    
+    
   }
-  strsplit_playlist <- function(dataframe){
-    dataframe$In_playlists <- strsplit(dataframe$In_playlists,";")
-    dataframe
-  }
-  cbind(Str_his_df,In_which_playlist) %>%
-  strsplit_playlist
+  mutate(str_his_comp, "In Playlist" = lapply(1:nrow(str_his_comp), in_which_playlists))
+  
 }
 
 how_many_skipped <- function(streaming_history, start_date, end_date, as_percentage = FALSE){
@@ -169,3 +163,5 @@ most_skipped_artist <- function(streaming_history, start_date, end_date, how_man
   df <- df[order(-df[["number"]]),]
   df[1:how_many,]
 }
+
+
