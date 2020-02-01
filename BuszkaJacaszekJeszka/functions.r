65eab8d2b6f9b57ed2a0768c2654354f40a6e55e
wicix <Wiktorj99@gmail.com>
2020-02-01 11:15:55 +0100

    this commit edits reading functions

--- BuszkaJacaszekJeszka/functions.r
+++ BuszkaJacaszekJeszka/functions.r
@@ -2,15 +2,20 @@ library(jsonlite)
 library(lubridate)
 library(dplyr)
 library(tidyr)
-
-##returns dataframe with data from spotify
-streaming_history_df <- function(folder_path){
-  file_paths<- list.files(folder_path,"StreamingHistory")
-  read_files <- lapply(file_paths, jsonlite::fromJSON)
+##returns dataframe with streaming history data from spotify
+Streaming_History_df <- function(folder_path){
+  files_path <- list.files(folder_path,"StreamingHistory")
+  if (folder_path != ".")
+    files_path <- paste(folder_path, files_path, sep = "/")
+  read_files <- lapply(files_path, jsonlite::fromJSON)
   bind_rows(read_files)
 }
 
-
+##changes names 
+names_change <- function(streaming_history, column_names = c("end_time", "artist_name", "track_name", "s_played")){
+  names(streaming_history) <- column_names
+  streaming_history
+}
 
 ##returns streaming history with end time changed to date
 #mutate(streaming_history, end_time = ymd_hm(end_time))
@@ -18,10 +23,6 @@ streaming_history_df <- function(folder_path){
 ##returns streaming_history with end_time column changed from character to date
 #mutate(streaming_history, s_played = dmilliseconds((s_played)))
 
-names_change <- function(streaming_history, column_names = c("end_time", "artist_name", "track_name", "s_played")){
-  names(streaming_history) <- column_names
-  streaming_history
-}
 ##returns streaming_history with added start_time column using end_time and ms_played
 add_start_time <- function(streaming_history){
   
@@ -35,34 +36,42 @@ add_start_time <- function(streaming_history){
 add_skipped <- function(streaming_history){
   skipped <- (streaming_history[["s_played"]] < duration(10, "seconds"))
   streaming_history <- cbind(streaming_history, skipped)
+  streaming_history
 }
 
 ##returns streaming history with added weekdays column
 add_weekday <- function(streaming_history){
   weekday <- wday(streaming_history[["start_time"]], label = TRUE)
   streaming_history <- cbind(streaming_history, weekday)
+  streaming_history
 }
 
 #### creating and preparing dataframe
-streaming_history_complete <- function(folder_path){
-  streaming_history_df(folder_path) %>%
-  names_change() %>% 
-  mutate(end_time = ymd_hm(end_time)) %>% 
-  mutate(s_played = dmilliseconds((s_played))) %>%
-  add_start_time() %>%
-  add_skipped() %>%
-  add_weekday()
+Streaming_History_Complete <- function(folder_path){
+  Streaming_History_df(folder_path) %>%
+    names_change() %>% 
+    mutate(end_time = ymd_hm(end_time)) %>% 
+    mutate(s_played = dmilliseconds((s_played))) %>%
+    add_start_time() %>%
+    add_skipped() %>%
+    add_weekday()
+  
 }
+
 ### creating dataframe with date, devices and country 
-search_queries_df <- function(folder_path){
-  file_path <-list.files(folder_path,"SearchQueries")
-  select(jsonlite::fromJSON(file_path),1:3) %>%
-  mutate(date = ymd(date))
-}
+Search_Queries_df <- function(folder_path){
+    files_path <-list.files(folder_path,"SearchQueries")
+    if (folder_path != ".")
+      files_path <- paste(folder_path, files_path, sep = "/")
+    list_of_df <- lapply(files_path, jsonlite::fromJSON)
+    df <- bind_rows(list_of_df)
+    df <- select(df, 1:3)
+    df <- mutate(df, date = ymd(date))
+  }
 
 
-
-### creating dataframe with names of playlists, lists with character of song names, and artist"
+### creating dataframe with names of playlists, string containing song names separated 
+#by ";;;" and artist names separated by ";;;"
 playlist_df_function <- function(folder_path) {
   song_names_function <- function(x)
     return(df[[2]][[x]][[1]][[1]])
@@ -83,7 +92,7 @@ playlist_df_function <- function(folder_path) {
 }
 
 # creating dataframe similar to Streaming_History_Complete, but this one has additional 
-#column with lists of playlists that including that song
+#column which has string of playlists that including that song, separated by ;
 str_his_with_playlists <- function(folder_path) {
   playlist_df <- playlist_df_function(folder_path)
   str_his_comp <- streaming_history_complete(folder_path)
@@ -109,6 +118,13 @@ str_his_with_playlists <- function(folder_path) {
   
 }
 
+
+
+
+
+## functions to be used on streaming history complete
+
+#how many songs were skipped in given time period, as a number or as percentage
 how_many_skipped <- function(streaming_history, start_date, end_date, as_percentage = FALSE){
   filtered <- filter(streaming_history, start_time >= ymd(start_date), start_time <= ymd(end_date), skipped == TRUE)  
   if(as_percentage) {
@@ -164,3 +180,13 @@ most_skipped_artist <- function(streaming_history, start_date, end_date, how_man
 }
 
 
+Search_Queries_df <- function(folder_path){
+  file_path <-list.files(folder_path,"SearchQueries")
+  file_paths <- paste(folder_path, file_path, sep = "/")
+  list_of_df <- lapply(file_paths, jsonlite::fromJSON)
+  df <- bind_rows(list_of_df)
+  df <- select(df, 1:3)
+  df <- mutate(df, date = ymd(date))
+}
+
+           
